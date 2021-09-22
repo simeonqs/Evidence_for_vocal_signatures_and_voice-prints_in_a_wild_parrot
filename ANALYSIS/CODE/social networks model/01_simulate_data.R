9ffc473210b69b8f3d287847016c3ff0c59177cf
Simeon Smeele <simeon_smeele@ecocn03.eva.mpg.de>
2021-09-22 12:00:41 +0200

    Minor update.

--- ANALYSIS/CODE/social networks model/01_simulate_data.R
+++ ANALYSIS/CODE/social networks model/01_simulate_data.R
@@ -1,7 +1,7 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: chapter II
 # Date started: 24-08-2021
-# Date last modified: 03-09-2021
+# Date last modified: 08-09-2021
 # Author: Simeon Q. Smeele
 # Description: Simple simulation of data that we might get from dtw. 
 # This version adds data to code whether or not an ind pair is the same ind. 
@@ -18,13 +18,13 @@ for(i in libraries){
 }
 
 # Settings
-set.seed(1)
-settings = list(N_ind = 5,
-                N_var = 5,
-                lambda_rec = 2,
+set.seed(2)
+settings = list(N_ind = 7,
+                N_var = 2,
+                lambda_rec = 5,
                 lambda_obs = 3,
-                sigma_ind = 1,
-                sigma_rec = 0.1,
+                sigma_ind = 0.001,
+                sigma_rec = 1,
                 sigma_obs = 0.1)
 
 # Paths
@@ -35,11 +35,11 @@ path_out = 'ANALYSIS/RESULTS/social networks model/sim_dat.RData'
 .functions = sapply(list.files(path_functions, pattern = '*R', full.names = T), source)
 
 # Simulate
-clean_dat = sim.sna.data(settings, plot_it = T)
+clean_dat = sim.sn.data(settings, plot_it = T)
 
 # Save
 save(clean_dat, file = path_out)
 
 # Report
-message(sprintf('Simulated data for %s individuals. Saved a total of %s data points',
-                clean_dat$N_ind, clean_dat$N_obs))
+message(sprintf('Simulated %s calls. Saved a total of %s data points',
+                clean_dat$N_calls, clean_dat$N_obs))
