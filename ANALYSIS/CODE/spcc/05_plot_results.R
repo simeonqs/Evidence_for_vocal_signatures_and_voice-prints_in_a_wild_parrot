68e1d8ea4d7b7c87582cf8828e2b36aceea19931
Simeon Smeele <simeon_smeele@ecocn03.eva.mpg.de>
2021-09-22 12:02:15 +0200

    Minor updates.

--- ANALYSIS/CODE/spcc/05_plot_results.R
+++ ANALYSIS/CODE/spcc/05_plot_results.R
@@ -1,7 +1,7 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: voice paper
 # Date started: 27-08-2021
-# Date last modified: 27-08-2021
+# Date last modified: 07-09-2021
 # Author: Simeon Q. Smeele
 # Description: Plotting the output per call type. 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -19,25 +19,25 @@ rm(list=ls())
 # Paths
 path_models = 'ANALYSIS/RESULTS/spcc/models'
 path_functions = 'ANALYSIS/CODE/functions'
-path_pdf = 'ANALYSIS/RESULTS/spcc/sna results.pdf'
+path_pdf = 'ANALYSIS/RESULTS/spcc/sn results.pdf'
 
 # Import functions
 .functions = sapply(list.files(path_functions, pattern = '*R', full.names = T), source)
 
 # Plot beta parameter per call type
-pdf(path_pdf, 10, 10)
+# pdf(path_pdf, 10, 10)
 par(mfrow = c(3, 3))
 for(path_model in list.files(path_models, full.names = T, pattern = '*RData')){
   load(path_model)
   name_data = path_model %>% str_remove(paste0(path_models, '/')) %>% str_remove('.RData')
   post = extract.samples(model)
-  prior = rnorm(1e6, 0, 0.5) %>% density
-  plot(prior, xlim = c(-2, 2), ylim = c(0, 3), main = name_data, 
+  prior = rnorm(1e6, 0, 1) %>% density
+  plot(prior, xlim = c(-2, 2), ylim = c(0, 5), main = name_data, 
        xlab = '', ylab = 'density')
   polygon(prior, col = alpha('grey', 0.5))
-  post$b_ind_pair %>% density %>% polygon(col = alpha(4, 0.8))
+  post$b_bar %>% density %>% polygon(col = alpha(4, 0.8))
 }
-dev.off()
+# dev.off()
 
 
 
