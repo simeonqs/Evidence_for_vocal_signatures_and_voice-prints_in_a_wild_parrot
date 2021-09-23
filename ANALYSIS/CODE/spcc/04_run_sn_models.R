d1d57dd294fe5f155c265eed943fa354ae6daadb
Simeon Smeele <simeon_smeele@ecocn03.eva.mpg.de>
2021-09-23 08:49:43 +0200

    Minor update.

--- ANALYSIS/CODE/spcc/04_run_sn_models.R
+++ ANALYSIS/CODE/spcc/04_run_sn_models.R
@@ -1,7 +1,7 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: voice paper
 # Date started: 27-08-2021
-# Date last modified: 06-09-2021
+# Date last modified: 22-09-2021
 # Author: Simeon Q. Smeele
 # Description: Running sna model per call type. 
 # NOTE: subsetting per call type for now!
@@ -20,9 +20,12 @@ rm(list=ls())
 
 # Paths
 path_data = 'ANALYSIS/RESULTS/spcc/datasets per call type'
-path_model = 'ANALYSIS/CODE/social networks model/m_8.stan'
+path_model = 'ANALYSIS/CODE/social networks model/m_10.stan'
 path_functions = 'ANALYSIS/CODE/functions'
 path_out = 'ANALYSIS/RESULTS/spcc/models'
+
+# Settings
+N_obs = 200
 
 # Import functions
 .functions = sapply(list.files(path_functions, pattern = '*R', full.names = T), source)
@@ -32,4 +35,4 @@ data_set_paths = list.files(path_data, full.names = T, pattern = '*RData')
 
 # Run models
 set.seed(1)
-.out = lapply(data_set_paths, run.sn.model, path_model, path_out, N_obs = 200)
+.out = lapply(data_set_paths, run.sn.model, path_model, path_out, N_obs = N_obs)
