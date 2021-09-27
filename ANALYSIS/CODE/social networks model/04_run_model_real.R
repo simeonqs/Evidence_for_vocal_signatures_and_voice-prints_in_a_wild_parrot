3361a94756bcde6b4cc6eb1130ec7eccd1731ec3
Simeon <simeonqs@hotmail.com>
2021-09-27 14:33:36 +0200

    Running model with and without time now.

--- ANALYSIS/CODE/social networks model/04_run_model_real.R
+++ ANALYSIS/CODE/social networks model/04_run_model_real.R
@@ -1,7 +1,7 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: chapter II
 # Date started: 24-08-2021
-# Date last modified: 31-08-2021
+# Date last modified: 24-09-2021
 # Author: Simeon Q. Smeele
 # Description: Running model on real data.  
 # This version was moved to the new repo and paths are fixed. 
@@ -20,25 +20,33 @@ rm(list = ls())
 
 # Paths
 path_data = 'ANALYSIS/RESULTS/social networks model/real_dat.RData'
-path_model = 'ANALYSIS/CODE/social networks model/m_10.stan'
+path_model_10 = 'ANALYSIS/CODE/social networks model/m_10.stan'
+path_model_13 = 'ANALYSIS/CODE/social networks model/m_13.stan'
 path_out = 'ANALYSIS/RESULTS/social networks model/real_model.RData'
+path_out_time = 'ANALYSIS/RESULTS/social networks model/real_model_time.RData'
 
 # Load data
 load(path_data)
 
 # Print
-message(sprintf('Starting model with %s observations.', clean_dat$N_obs))
+message(sprintf('Starting models with %s observations.', clean_dat$N_obs))
 
-# Run model
-model = stan(path_model,
+# Run models
+model = stan(path_model_10,
              data = clean_dat, 
              chains = 4, cores = 4,
              iter = 2000, warmup = 500,
              control = list(max_treedepth = 15, adapt_delta = 0.95))
-
-# Save
 save('model', file = path_out)
 
+model = stan(path_model_13,
+             data = clean_dat, 
+             chains = 4, cores = 4,
+             iter = 2000, warmup = 500,
+             control = list(max_treedepth = 15, adapt_delta = 0.95))
+save('model', file = path_out_time)
+
+
 # Print the results
-message('Here are the results: \n')
+message('Here are the results of the time model: \n')
 print(precis(model, depth = 1))
