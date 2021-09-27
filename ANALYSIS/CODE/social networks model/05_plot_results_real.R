6fe548f967dda923e9ddc27a1e92483952816670
Simeon <simeonqs@hotmail.com>
2021-09-27 14:33:49 +0200

    Updating plotting to fit new model structure.

--- ANALYSIS/CODE/social networks model/05_plot_results_real.R
+++ ANALYSIS/CODE/social networks model/05_plot_results_real.R
@@ -24,40 +24,15 @@ path_pdf = 'ANALYSIS/RESULTS/social networks model/real_results.pdf'
 # Load data
 load(path_data)
 load(path_model)
+post = extract.samples(model)
 
 # Plot
-post = extract.samples(model)
-pdf(path_pdf)
-per_pair = lapply(1:ncol(post$z_ind_pair), function(pair)
-  sapply(1:nrow(post$z_ind_pair), function(i)
-    post$a[i] + post$z_ind_pair[i, pair] * post$sigma_ind_pair[i]))
-ind_same = sapply(1:max(clean_dat$ind_pair), function(pair) 
-  ifelse(clean_dat$ind_i[clean_dat$ind_pair == pair][1] == 
-           clean_dat$ind_j[clean_dat$ind_pair == pair][1], 1, 2))
-plot(NULL, xlim = c(0, max(clean_dat$ind_pair)), ylim = c(-3, 3))
-for(pair in 1:max(clean_dat$ind_pair)) 
-  lines(rep(pair, 2), PI(per_pair[[pair]]), lwd = 3, col = alpha(ind_same[pair], 0.8))
-points(sapply(per_pair, mean), 
-       col = alpha(ind_same, 0.8),
-       pch = 16)
-plot(precis(model))
-dev.off()
+# pdf(path_pdf)
+prior = rnorm(1e6, 0, 1) %>% density
+plot(prior, xlim = c(-2, 2), ylim = c(0, 5), main = '', 
+     xlab = '', ylab = 'density')
+polygon(prior, col = alpha('grey', 0.5))
+post$b_bar_rec %>% density %>% polygon(col = alpha(2, 0.8))
+post$b_bar_ind %>% density %>% polygon(col = alpha(4, 0.8))
+# dev.off()
 
-# Load data
-load(path_data)
-load(path_model)
-
-# Plot
-post = extract.samples(model)
-per_pair = lapply(1:ncol(post$z_ind_pair), function(pair)
-  sapply(1:nrow(post$z_ind_pair), function(i)
-    post$a[i] + post$z_ind_pair[i, pair] * post$sigma_ind_pair[i]))
-ind_same = sapply(1:max(clean_dat$ind_pair), function(pair) 
-  ifelse(clean_dat$ind_i[clean_dat$ind_pair == pair][1] == 
-           clean_dat$ind_j[clean_dat$ind_pair == pair][1], 1, 2))
-plot(NULL, xlim = c(0, max(clean_dat$ind_pair)), ylim = c(-3, 3))
-for(pair in 1:max(clean_dat$ind_pair)) 
-  lines(rep(pair, 2), PI(per_pair[[pair]]), lwd = 3, col = alpha(ind_same[pair], 0.8))
-points(sapply(per_pair, mean), 
-       col = alpha(ind_same, 0.8),
-       pch = 16)
