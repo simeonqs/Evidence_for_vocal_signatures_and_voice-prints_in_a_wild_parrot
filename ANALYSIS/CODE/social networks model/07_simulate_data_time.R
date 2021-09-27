b7ea1eff650a0474af98fcdf43b0e7e5a7bda52a
Simeon <simeonqs@hotmail.com>
2021-09-27 14:34:05 +0200

    Minor update.

--- ANALYSIS/CODE/social networks model/07_simulate_data_time.R
+++ ANALYSIS/CODE/social networks model/07_simulate_data_time.R
@@ -1,13 +1,9 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: chapter II
 # Date started: 24-08-2021
-# Date last modified: 08-09-2021
+# Date last modified: 24-09-2021
 # Author: Simeon Q. Smeele
-# Description: Simple simulation of data that we might get from dtw. 
-# This version adds data to code whether or not an ind pair is the same ind. 
-# This version adds the rec level. 
-# This version was moved to the new repo and paths were fixed. 
-# This version was moved to own location, paths were fixed and simulation moved to function. 
+# Description: Simple simulation of data that we might get from dtw including time. 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # Loading libraries
@@ -18,13 +14,15 @@ for(i in libraries){
 }
 
 # Settings
-set.seed(1)
-settings = list(N_ind = 7,
+set.seed(2)
+settings = list(N_ind = 4,
                 N_var = 2,
-                lambda_obs = 7,
+                lambda_obs = 4,
+                lambda_rec = 4,
                 sigma_ind = 1,
-                sigma_obs = 0.05,
-                slope_time = 0.5,
+                sigma_rec = 0.1,
+                sigma_obs = 0.01,
+                slope_time = 0.02,
                 dur_rec = 20)
 
 # Paths
@@ -36,10 +34,11 @@ path_out = 'ANALYSIS/RESULTS/social networks model/sim_dat_time.RData'
 
 # Simulate
 clean_dat = sim.sn.data.time(settings, plot_it = T)
+clean_dat$time = clean_dat$time/60
 
 # Save
 save(clean_dat, file = path_out)
 
 # Report
 message(sprintf('Simulated %s calls. Saved a total of %s data points',
-                clean_dat$N_calls, clean_dat$N_obs))
+                clean_dat$N_call, clean_dat$N_obs))
