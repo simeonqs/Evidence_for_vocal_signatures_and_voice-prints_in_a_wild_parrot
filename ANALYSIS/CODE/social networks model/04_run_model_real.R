c3245df2e7dcb2e208696ef64cbd1ee79a140ecf
Simeon <simeonqs@hotmail.com>
2021-09-30 08:49:24 +0200

    Minor update.

--- ANALYSIS/CODE/social networks model/04_run_model_real.R
+++ ANALYSIS/CODE/social networks model/04_run_model_real.R
@@ -22,8 +22,10 @@ rm(list = ls())
 path_data = 'ANALYSIS/RESULTS/social networks model/real_dat.RData'
 path_model_10 = 'ANALYSIS/CODE/social networks model/m_10.stan'
 path_model_13 = 'ANALYSIS/CODE/social networks model/m_13.stan'
+path_model_14 = 'ANALYSIS/CODE/social networks model/m_14.stan'
 path_out = 'ANALYSIS/RESULTS/social networks model/real_model.RData'
 path_out_time = 'ANALYSIS/RESULTS/social networks model/real_model_time.RData'
+path_out_all = 'ANALYSIS/RESULTS/social networks model/real_model_all.RData'
 
 # Load data
 load(path_data)
@@ -33,19 +35,25 @@ message(sprintf('Starting models with %s observations.', clean_dat$N_obs))
 
 # Run models
 model = stan(path_model_10,
-             data = clean_dat, 
+             data = clean_dat,
              chains = 4, cores = 4,
              iter = 2000, warmup = 500,
              control = list(max_treedepth = 15, adapt_delta = 0.95))
 save('model', file = path_out)
 
 model = stan(path_model_13,
-             data = clean_dat, 
+             data = clean_dat,
              chains = 4, cores = 4,
              iter = 2000, warmup = 500,
              control = list(max_treedepth = 15, adapt_delta = 0.95))
 save('model', file = path_out_time)
 
+model = stan(path_model_14,
+             data = clean_dat, 
+             chains = 4, cores = 4,
+             iter = 2000, warmup = 500,
+             control = list(max_treedepth = 15, adapt_delta = 0.95))
+save('model', file = path_out_all)
 
 # Print the results
 message('Here are the results of the time model: \n')
