44a2e2dd05474288cb8823b0c060b7783190b310
Simeon <simeonqs@hotmail.com>
2021-10-17 11:55:55 +0200

    Minor update.

--- ANALYSIS/CODE/01_time_effect/01_prepare_data_dates.R
+++ ANALYSIS/CODE/01_time_effect/01_prepare_data_dates.R
@@ -30,27 +30,28 @@ path_out = 'ANALYSIS/RESULTS/01_time_effect/data_sets_dates.RData'
 prep.dat = function(file){
   print(file)
   load(file)
-  if(nrow(d_sub) > 200) subber = sample(nrow(d_sub), 200) else subber = sample(nrow(d_sub))
+  if(nrow(d_sub) > 100) subber = sample(nrow(d_sub), 100) else subber = sample(nrow(d_sub))
   inds = as.integer(as.factor(d_sub$ind[subber]))
   recs = as.integer(as.factor(d_sub$file[subber]))
-  dates = d_sub$files %>% str_sub(1, 10) %>% str_replace_all('_', '-') %>% as.Date %>% as.numeric
-  if(length(time_saver) == 0) time_saver = d_sub$time[subber]/60/60
-  clean_dat = m.to.df(m_sub[subber, subber], inds, recs, date_saver = dates, clean_data = T)
-  subber = which(clean_dat$same_rec[clean_dat$rec_pair] == 1)
-  if(any(clean_dat$ind_i[subber] != clean_dat$ind_j[subber])) stop('Problem subsetting!')
-  sub_dat = list(call_i = clean_dat$call_i[subber] %>% as.factor %>% as.integer,
-                 call_j = clean_dat$call_j[subber] %>% as.factor %>% as.integer,
-                 rec_pair = clean_dat$rec_pair[subber] %>% as.factor %>% as.integer,
-                 ind = clean_dat$ind_i[subber] %>% as.factor %>% as.integer,
-                 time = clean_dat$date[subber],
-                 d = clean_dat$d[subber] %>% scale %>% as.numeric,
-                 N_obs = length(subber),
-                 N_call = length(unique(c(clean_dat$call_i[subber],
-                                          clean_dat$call_j[subber]))),
-                 N_rec = length(unique(clean_dat$rec_pair[subber])),
-                 N_ind = length(unique(clean_dat$ind_i[subber])),
-                 settings = clean_dat$settings)
-  plot(sub_dat$date, sub_dat$d, col = sub_dat$ind, pch = 16)
+  dates = d_sub$file %>% str_sub(1, 10) %>% str_replace_all('_', '-') %>% as.Date %>% as.numeric
+  clean_dat = m.to.df(m_sub[subber, subber], inds, recs, day_saver = dates, clean_data = T)
+  subber = which(clean_dat$same_rec[clean_dat$rec_pair] == 0 & clean_dat$same_ind[clean_dat$ind_pair] == 1)
+  if(length(subber) == 0) return(NA) else {
+    if(any(clean_dat$ind_i[subber] != clean_dat$ind_j[subber])) stop('Problem subsetting!')
+    sub_dat = list(call_i = clean_dat$call_i[subber] %>% as.factor %>% as.integer,
+                   call_j = clean_dat$call_j[subber] %>% as.factor %>% as.integer,
+                   rec_pair = clean_dat$rec_pair[subber] %>% as.factor %>% as.integer,
+                   ind = clean_dat$ind_i[subber] %>% as.factor %>% as.integer,
+                   date = clean_dat$date[subber]/30,
+                   d = clean_dat$d[subber] %>% scale %>% as.numeric,
+                   N_obs = length(subber),
+                   N_call = length(unique(c(clean_dat$call_i[subber],
+                                            clean_dat$call_j[subber]))),
+                   N_rec = length(unique(clean_dat$rec_pair[subber])),
+                   N_ind = length(unique(clean_dat$ind_i[subber])),
+                   settings = clean_dat$settings)
+    plot(sub_dat$date, sub_dat$d, col = sub_dat$ind, pch = 16, main = file)
+  }
   return(sub_dat)
 }
 
