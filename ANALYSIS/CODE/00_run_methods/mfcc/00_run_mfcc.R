2d57d352c5f9fe27bdb21c5333d2b80f1abff3a5
Simeon <simeonqs@hotmail.com>
2021-10-21 15:54:43 +0200

    Saving objects together.

--- ANALYSIS/CODE/00_run_methods/mfcc/00_run_mfcc.R
+++ ANALYSIS/CODE/00_run_methods/mfcc/00_run_mfcc.R
@@ -1,10 +1,11 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: voice paper
 # Date started: 22-09-2021
-# Date last modified: 13-10-2021
+# Date last modified: 20-10-2021
 # Author: Simeon Q. Smeele
 # Description: Running mfcc per call type and saving data as long distance for SN model. 
-# source('ANALYSIS/CODE/mfcc/00_run_mfcc.R')
+# This version saves objects together. 
+# source('ANALYSIS/CODE/00_run_methods/mfcc/00_run_mfcc.R')
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -24,11 +25,8 @@ rm(list=ls())
 # Paths
 path_functions = 'ANALYSIS/CODE/functions'
 path_audio = 'ANALYSIS/DATA/audio'
-path_selection_tables = 'ANALYSIS/DATA/selection tables'
-path_annotations = 'ANALYSIS/DATA/overview recordings/annotations.csv'
-path_context = 'ANALYSIS/DATA/overview recordings/call types.xlsx'
-path_out = 'ANALYSIS/RESULTS/mfcc/datasets per call type'
-path_call_type_classification = 'ANALYSIS/CODE/call type classification.R'
+path_out = 'ANALYSIS/RESULTS/00_run_methods/mfcc/results.RData'
+path_data_sets = 'ANALYSIS/RESULTS/00_run_methods/data_sets.RData'
 
 # Import functions
 .functions = sapply(list.files(path_functions, pattern = '*R', full.names = T), source)
@@ -36,59 +34,69 @@ path_call_type_classification = 'ANALYSIS/CODE/call type classification.R'
 # Audio files 
 audio_files = list.files(path_audio,  '*wav', full.names = T)
 
-# Load selection tables
-st = load.selection.tables(path_selection_tables, path_annotations, path_context)
+# Load data
+load(path_data_sets)
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # ANALYSIS: all calls ----
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-# Get better start and end times
-new_sel_table = wave.detec(st, path_audio, bp = 0.3)
+# Get better start and end times - keep times isolated contact calls
+data_sets_updated = lapply(data_sets[-length(data_sets)], wave.detec, path_audio, bp = 0.3)
+data_sets_updated = append(data_sets_updated, data_sets[length(data_sets)])
+names(data_sets_updated) = names(data_sets)
 
 # Test how well it worked
+## not working well for growly calls and combined calls
 if(F){
   pdf('~/Desktop/test.pdf')
   set.seed(1)
-  for(i in sample(seq_along(new_sel_table), 10)){
-    wave = readWave(paste0(path_audio, '/', new_sel_table$file[i], '.wav'), 
-                    from = new_sel_table$Begin.Time..s.[i] - 0.1, to = new_sel_table$End.Time..s.[i] + 0.1, 
-                    units = 'seconds')
-    better.spectro(wave)
-    abline(v = 0.1, lty = 2, lwd = 3)
-    abline(v = length(wave@left)/wave@samp.rate - 0.1, lty = 2, lwd = 3)
+  for(nr in 1:length(data_sets_updated)){
+    for(i in sample(seq_along(data_sets_updated[[nr]]), 10)){
+      wave = readWave(paste0(path_audio, '/', data_sets_updated[[nr]]$file[i], '.wav'), 
+                      from = data_sets_updated[[nr]]$Begin.Time..s.[i] - 0.1, 
+                      to = data_sets_updated[[nr]]$End.Time..s.[i] + 0.1, 
+                      units = 'seconds')
+      better.spectro(wave)
+      abline(v = 0.1, lty = 2, lwd = 3)
+      abline(v = length(wave@left)/wave@samp.rate - 0.1, lty = 2, lwd = 3)
+    }
   }
   dev.off()
 }
 
-# Run mfcc
-out = data.frame()
-for(i in 1:nrow(new_sel_table)){
-  wave = readWave(paste0(path_audio, '/', new_sel_table$file[i], '.wav'), 
-                  new_sel_table$Begin.Time..s.[i], new_sel_table$End.Time..s.[i], 
-                  units = 'seconds')
+# Functions to run mfcc
+run.mfcc = function(path_audio_file, 
+                    from,
+                    to){
+  wave = load.wave(path_audio_file, 
+                   from,
+                   to)
   melfcc_out = suppressWarnings(
     melfcc(wave, wintime = 512/44100, hoptime = 50/44100, numcep = 20, minfreq = 300)
   )
   means = melfcc_out %>% apply(2, mean)
   sds = melfcc_out %>% apply(2, sd)
-  new = as.data.frame(t(means))
-  names(new) = paste('V', names(new))
-  new = cbind(new, as.data.frame(t(sds)))
-  out = rbind(out, new)
+  out = as.data.frame(t(means))
+  names(out) = paste('V', names(out))
+  return(cbind(out, as.data.frame(t(sds))))
 }
 
-# Listing the call types to include - need to include more, just starting small
-source(path_call_type_classification)
-if(length(unique(new_sel_table$`call type`)) != length(unique(unlist(types_include)))) 
-  warning('Not all call types are included.')
-
-# Run through them and save
-for(type in names(types_include)){
-  out_sub = out[new_sel_table$`call type` %in% types_include[[type]],]
-  d_sub = new_sel_table[new_sel_table$`call type` %in% types_include[[type]],]
-  names(d_sub)[names(d_sub) == 'bird'] = 'ind'
-  m_sub = as.matrix(dist(out_sub))
-  m_sub = m_sub/max(m_sub)
-  save(d_sub, m_sub, file = sprintf('%s/%s.RData', path_out, type))
+run.mfcc.multiple = function(data_set){
+  out = lapply(1:nrow(data_set), function(i) run.mfcc(paste0(path_audio, '/', data_set$file[i], '.wav'), 
+                                                      data_set$Begin.Time..s.[i], 
+                                                      data_set$End.Time..s.[i]))
+  return(bind_rows(out))
 }
+
+
+# Run mfcc
+mfcc_out = lapply(data_sets_updated, run.mfcc.multiple)
+
+# Calculate distance matrices
+## make sure this dist is correct - move function out and write unit tests
+mfcc_dists = lapply(mfcc_out, function(x) as.matrix(dist(scale(x[,-1]))))
+
+# Save
+save(mfcc_out, mfcc_dists, data_sets, file = path_out)
+message('Done.')
--- ANALYSIS/CODE/00_run_methods/spcc/02_create_data_sets.R
+++ ANALYSIS/CODE/00_run_methods/spcc/02_create_data_sets.R
@@ -5,7 +5,7 @@
 # Author: Simeon Q. Smeele
 # Description: Creates a dataset per call type including the subsetted distance matrix from spcc, and a 
 # data.frame with file, file_sel, and ind. 
-# source('ANALYSIS/CODE/spcc/02_create_data_sets.R')
+# This version uses the preprocessed datasets. 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # Loading libraries
@@ -20,36 +20,25 @@ rm(list=ls())
 
 # Paths
 path_functions = 'ANALYSIS/CODE/functions'
-path_o = 'ANALYSIS/RESULTS/spcc/o_with_names.RData'
-path_out = 'ANALYSIS/RESULTS/spcc/datasets per call type'
-path_selection_tables = 'ANALYSIS/DATA/selection tables'
-path_annotations = 'ANALYSIS/DATA/overview recordings/annotations.csv'
-path_context = 'ANALYSIS/DATA/overview recordings/call types.xlsx'
-path_call_type_classification = 'ANALYSIS/CODE/call type classification.R'
+path_o = 'ANALYSIS/RESULTS/00_run_methods/spcc/o_with_names.RData'
+path_data_sets = 'ANALYSIS/RESULTS/00_run_methods/data_sets.RData'
+path_out = 'ANALYSIS/RESULTS/00_run_methods/spcc/results.RData'
 
 # Import functions
 .functions = sapply(list.files(path_functions, pattern = '*R', full.names = T), source)
 
 # Load data 
+load(path_data_sets)
 load(path_o)
-st = load.selection.tables(path_selection_tables, path_annotations, path_context)
 
 # Create dist mat
 m = o.to.m(o_with_names$o, o_with_names$file_sels)
+names(m) = o_with_names$file_sels
 
-# Get files and inds
-d = data.frame(file_sel = o_with_names$file_sels,
-               file = o_with_names$file_sels %>% strsplit('-') %>% sapply(`[`, 1))
-d$ind = sapply(d$file_sel, function(fs) st$bird[st$fs == fs])
-d$time = sapply(d$file_sel, function(fs) st$Begin.Time..s.[st$fs == fs])
+# Subset
+spcc_dists = lapply(data_sets, function(data_set) m[data_set$fs, data_set$fs])
+names(spcc_dists) = names(data_sets)
 
-# Listing the call types to include - need to include more, just starting small
-source(path_call_type_classification)
-
-# Run through them and save
-for(type in names(types_include)){
-  subber = d$file_sel %in% st$fs[st$`call type` %in% types_include[[type]]]
-  d_sub = d[subber,]
-  m_sub = m[subber, subber]
-  save(d_sub, m_sub, file = sprintf('%s/%s.RData', path_out, type))
-}
+# Save
+save(spcc_dists, data_sets, file = path_out)
+message('Done.')
