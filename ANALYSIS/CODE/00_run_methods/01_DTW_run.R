2cdafdbed5fbd109c76e3cc189039bbfbd179ecd
Simeon <simeonqs@hotmail.com>
2022-01-18 16:36:00 +0100

    Fix to also first subset the data sets that make sense for DTW.

--- ANALYSIS/CODE/00_run_methods/01_DTW_run.R
+++ ANALYSIS/CODE/00_run_methods/01_DTW_run.R
@@ -20,43 +20,30 @@ rm(list=ls())
 # Paths
 path_data = 'ANALYSIS/RESULTS/00_run_methods/all_data.RData'
 path_functions = 'ANALYSIS/CODE/functions'
-path_dtw_out = 'ANALYSIS/RESULTS/luscinia/dtw/m.RData'
+path_dtw_out = 'ANALYSIS/RESULTS/00_run_methods/dtw/m_list.RData'
 
 # Load data
 load(path_data)
 
+# Which data sets make sense for DTW
+data_sets = data_sets[c('contact', 'contact_mix', 
+                        'short_contact', 'trruup', 
+                        'kaw', 'tja', 'tjup', 
+                        'tonal_mix')]
+
 # Subset smooth traces to only include call types for which DTW makes sense
 smooth_traces = smooth_traces[which(names(smooth_traces) %in% 
-                                      unlist(data_sets[c('contact', 'contact_mix', 
-                                                         'short_contact', 'trruup', 
-                                                         'kaw', 'tja', 'tjup', 
-                                                         'tonal_mix', 'isolated_contact')]))]
+                                      unlist(data_sets))]
 
 # Import functions
 .functions = sapply(list.files(path_functions, pattern = '*R', full.names = T), source)
 
-# Running through the combinations -  DTW
-l = length(smooth_traces)
-c = combn(1:l, 2)
-message(sprintf('Starting DTW for %s traces...', l))
-out = mclapply(1:ncol(c), function(x) {
-  i = c[1,x]
-  j = c[2,x]
-  dtw_out = dtw(smooth_traces[[i]], smooth_traces[[j]])
-  return( dtw_out$normalizedDistance )
-}, mc.cores = 4) %>% unlist # end running through the combinations
-message('Done!')
-
-# Making it into a matrix
-o = out
-o = o / max(o)
-o = log(o)
-hist(o)
-m = o.to.m(o, names(smooth_traces))
-rownames(m) = colnames(m) = names(smooth_traces)
+# Run DTW per dataset
+m_list = lapply(data_sets, function(data_set) 
+  run.dtw(smooth_traces[which(names(smooth_traces) %in% data_set)]))
 
 # Save
-save(m, file = path_dtw_out)
+save(m_list, file = path_dtw_out)
 
 # Report
-message(sprintf('Saved dtw results for %s calls.', l))
+message(sprintf('Saved dtw results for %s data sets.', length(data_sets)))
