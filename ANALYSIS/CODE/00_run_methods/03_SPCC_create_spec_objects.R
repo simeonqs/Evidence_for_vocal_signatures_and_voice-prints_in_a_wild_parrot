4b8ca6e4194d770324c959807af31e01a7db5750
Simeon <simeonqs@hotmail.com>
2022-01-18 19:56:58 +0100

    Rename and update to data for 2021.

--- ANALYSIS/CODE/00_run_methods/03_SPCC_create_spec_objects.R
+++ ANALYSIS/CODE/00_run_methods/03_SPCC_create_spec_objects.R
@@ -1,11 +1,11 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: chapter II
 # Date started: 26-08-2021
-# Date last modified: 27-08-2021
+# Date last modified: 18-01-2022
 # Author: Simeon Q. Smeele
 # Description: Creating spec objects for all calls from 2020.
 # This version adds the names to the spec_objects. 
-# source('ANALYSIS/CODE/spcc/00_create_spec_objects.R')
+# This version is updated for the 2021 data. 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -14,8 +14,8 @@
 
 # Loading libraries
 libraries = c('tidyverse', 'warbleR', 'oce', 'signal')
-for(i in libraries){
-  if(! i %in% installed.packages()) lapply(i, install.packages)
+for(lib in libraries){
+  if(! lib %in% installed.packages()) lapply(lib, install.packages)
   lapply(libraries, require, character.only = TRUE)
 }
 
@@ -24,32 +24,30 @@ rm(list=ls())
 
 # Paths
 path_functions = 'ANALYSIS/CODE/functions'
+path_data = 'ANALYSIS/RESULTS/00_run_methods/all_data.RData'
 path_audio = 'ANALYSIS/DATA/audio'
-path_selection_tables = 'ANALYSIS/DATA/selection tables'
-path_annotations = 'ANALYSIS/DATA/overview recordings/annotations.csv'
-path_spec_objects = 'ANALYSIS/RESULTS/spcc/spec_objects.RData'
+path_spec_objects = 'ANALYSIS/RESULTS/00_run_methods/spcc/spec_objects.RData'
 
 # Import functions
 .functions = sapply(list.files(path_functions, pattern = '*R', full.names = T), source)
 
-# Audio files 
-audio_files = list.files(path_audio,  '*wav', full.names = T)
-
-# Load selection tables
-dat = load.selection.tables(path_selection_tables, path_annotations)
+# Load data
+load(path_data)
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # ANALYSIS: ----
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
+# Load waves
+waves = lapply(1:nrow(st), function(i)
+  load.wave(path_audio_file = paste0(path_audio, '/', st$file[i], '.wav'), 
+            from = st$Begin.Time..s.[i],
+            to = st$End.Time..s.[i]))
+
 # Generate spec_ojects
-spec_objects = sapply(1:nrow(dat), function(i){
-  new_dat = dat[i,]
-  file = audio_files[str_detect(audio_files, new_dat$file)]
-  wave = readWave(file, from = new_dat$Begin.Time..s., to = new_dat$End.Time..s., units = 'seconds')
-  if(max(abs(wave@left)) == 32768) wave@left = wave@right # if clipping use right channel
+spec_objects = sapply(waves, function(wave){
   wave = ffilter(wave, from = 500, output = 'Wave')
-  spec_oject = cutted.spectro(wave, freq_range = c(1000, 6000), plot_it = F, 
+  spec_oject = cutted.spectro(wave, freq_range = c(1000, 4000), plot_it = F, 
                               thr_low = 1.1, thr_high = 1.8,
                               wl = 512, ovl = 450, 
                               method = 'sd',
@@ -61,5 +59,5 @@ spec_objects = sapply(1:nrow(dat), function(i){
 image(t(spec_objects[[1]]), col = hcl.colors(12, 'Blue-Yellow', rev = T)) 
 
 # Save spec_objects
-names(spec_objects) = paste(dat$file, dat$Selection, sep = '-')
+names(spec_objects) = st$fs
 save(spec_objects, file = path_spec_objects)
--- ANALYSIS/CODE/00_run_methods/04_SPCC_run_pixel_comparison.R
+++ ANALYSIS/CODE/00_run_methods/04_SPCC_run_pixel_comparison.R
@@ -1,20 +1,19 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: chapter II
 # Date started: 26-08-2021
-# Date last modified: 27-08-2021
+# Date last modified: 18-01-2022
 # Author: Simeon Q. Smeele
 # Description: Running pixel comparison on all calls from 2020. 
 # Note: This script takes a long time. Best to run on HPC and use many cores.
 # Updates:
 # This version adds the file_sels to the o object. 
-# Source with:
-# source('ANALYSIS/CODE/spcc/01_run_pixel_comparison.R')
+# This version is updated to run with the 2021 data and run smoother. 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # Loading libraries
 libraries = c('tidyverse', 'parallel')
-for(i in libraries){
-  if(! i %in% installed.packages()) lapply(i, install.packages)
+for(lib in libraries){
+  if(! lib %in% installed.packages()) lapply(lib, install.packages)
   lapply(libraries, require, character.only = TRUE)
 }
 
@@ -22,23 +21,36 @@ for(i in libraries){
 rm(list=ls()) 
 
 # Settings
-n_cores = 40
+n_cores = 4
 
 # Paths
 path_functions = 'ANALYSIS/CODE/functions'
-path_spec_objects = 'ANALYSIS/RESULTS/spcc/spec_objects.RData'
-path_o = 'ANALYSIS/RESULTS/spcc/o_with_names.RData'
+path_spec_objects = 'ANALYSIS/RESULTS/00_run_methods/spcc/spec_objects.RData'
+path_out = 'ANALYSIS/RESULTS/00_run_methods/spcc/m_list.RData'
+path_data = 'ANALYSIS/RESULTS/00_run_methods/all_data.RData'
 
 # Import functions
 .functions = sapply(list.files(path_functions, pattern = '*R', full.names = T), source)
 
 # Load data 
 load(path_spec_objects)
+load(path_data)
 
 # Get combinations and run function
-c = combn(1:length(spec_objects), 2)
-o = mclapply(1:ncol(c), function(i) sliding.pixel.comparison(spec_objects[[c[1,i]]], spec_objects[[c[2,i]]]),
-             mc.cores = n_cores) %>% unlist
-o = o/max(o)
-o_with_names = list(o = o, file_sels = names(spec_objects))
-save(o_with_names, file = path_o)
+m_list = lapply(data_sets, function(data_set){
+  
+  spec_objects_sub = spec_objects[st$fs %in% data_set]
+  c = combn(1:length(spec_objects_sub), 2)
+  o = mclapply(1:ncol(c), function(i) 
+    sliding.pixel.comparison(spec_objects_sub[[c[1,i]]], spec_objects_sub[[c[2,i]]]),
+               mc.cores = n_cores) %>% unlist
+  o = o/max(o)
+  m = o.to.m(o, names(spec_objects_sub))
+  return(m)
+  
+})
+
+names(m_list) = names(data_sets)
+
+# Save
+save(m_list, file = path_out)
