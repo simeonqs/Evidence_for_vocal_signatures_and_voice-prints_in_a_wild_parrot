7777a8b52d546f4b8db60d0d119a27c1deac36cf
Simeon <simeonqs@hotmail.com>
2022-01-21 16:16:07 +0100

    Moving paths into seperate scripts.

---
+++ ANALYSIS/CODE/paths.R
@@ -0,0 +1,24 @@
+# This script can be sourced to have all the paths handy.
+
+path_functions = 'ANALYSIS/CODE/functions'
+
+path_data_sets_time = 'ANALYSIS/RESULTS/03_time_effect/data_sets_time.RData'
+path_data_sets_date = 'ANALYSIS/RESULTS/03_time_effect/data_sets_date.RData'
+
+path_model_10 = 'ANALYSIS/CODE/social networks model/m_10.stan'
+path_time_model = 'ANALYSIS/CODE/social networks model/m_time_2.stan'
+path_date_model = 'ANALYSIS/CODE/social networks model/m_date_2.stan'
+
+path_ind_model_results = 'ANALYSIS/RESULTS/02_compare_call_types/all_models_out.RData'
+path_time_model_results = 'ANALYSIS/RESULTS/03_time_effect/all_models_out_time.RData'
+path_date_model_results = 'ANALYSIS/RESULTS/03_time_effect/all_models_out_date.RData'
+
+path_data = 'ANALYSIS/RESULTS/00_run_methods/all_data.RData'
+
+path_dtw_m_list = 'ANALYSIS/RESULTS/00_run_methods/dtw/m_list.RData'
+path_spcc_m_list = 'ANALYSIS/RESULTS/00_run_methods/spcc/m_list.RData'
+path_mfcc_m_list = 'ANALYSIS/RESULTS/00_run_methods/mfcc/m_list.RData'
+path_specan_m_list = 'ANALYSIS/RESULTS/00_run_methods/specan/m_list.RData'
+
+path_pdf_time_results = 'ANALYSIS/RESULTS/03_time_effect/model results.pdf'
+
