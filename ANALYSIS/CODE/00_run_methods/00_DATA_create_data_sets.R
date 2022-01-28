b1785b8aa2ceed68df588bb1e72e67f71f3ac347
Simeon <simeonqs@hotmail.com>
2022-01-28 17:51:12 +0100

    Adding data from 2020 and renaming all objects. Moving many chunks into functions.

--- ANALYSIS/CODE/00_run_methods/00_DATA_create_data_sets.R
+++ ANALYSIS/CODE/00_run_methods/00_DATA_create_data_sets.R
@@ -1,7 +1,7 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: voice paper
 # Date started: 19-10-2021
-# Date last modified: 27-01-2022
+# Date last modified: 28-01-2022
 # Author: Simeon Q. Smeele
 # Description: Loading the selection tables and subsetting per call type. Saves subsetted data frames in 
 # one object to be used in further steps. 
@@ -9,6 +9,7 @@
 # This version adds the isolated contact calls that are also in Luscinia. 
 # This version switches to the 2021 data with all start and end coming from Luscinia. 
 # This version also reads in the waves and saves them in a separate object. 
+# This version loads data from both years and renames all objects with year subscript.
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # Loading libraries
@@ -28,80 +29,58 @@ source('ANALYSIS/CODE/paths.R')
 .functions = sapply(list.files(path_functions, pattern = '*R', full.names = T), source)
 
 # Load data 
-st = load.selection.tables(path_selection_tables, path_annotations_2021 = path_annotations_2021)
-traces = load.traces(path_traces_2021, path_bad_traces_2021)
+st_20 = load.selection.tables(path_selection_tables, path_annotations = path_annotations_2020,
+                              path_context = path_context)
+st_21 = load.selection.tables(path_selection_tables, path_annotations_2021 = path_annotations_2021)
+traces_20 = load.traces(path_traces_2020, path_bad_traces_2020)
+traces_21 = load.traces(path_traces_2021, path_bad_traces_2021)
 
-# Remove handling and release for now
-st = st[!str_detect(st$file, '10_26'),]
-st = st[!str_detect(st$file, '10_27'),]
+# Remove handling and release from 2021 data
+st_21 = st_21[!str_detect(st_21$file, '10_26'),]
+st_21 = st_21[!str_detect(st_21$file, '10_27'),]
 
 # Extract traces, smoothen and padd
-message(sprintf('Starting the smoothening of %s traces...', length(unique(traces$Song))))
-calls = unique(traces$Song)
-smooth_traces = mclapply(calls, function(call){
-  trace = traces$Fundamental_frequency[traces$Song == call]
-  time = traces$Time[traces$Song == call]
-  fit = gap.filler(time, trace)
-  new_trace = smooth.spline(fit, spar = 0.1) %>% fitted
-  return(new_trace)
-}, mc.cores = 4)
-names(smooth_traces) = str_remove(calls, '.wav')
-message('Done.')
+smooth_traces_20 = smooth.traces(traces_20)
+smooth_traces_21 = smooth.traces(traces_21)
 
-# Remove all calls that are not in Luscinia and print them
-traces$fs = str_remove(traces$Song, '.wav')
-not_luscinia = unique(st$fs[!st$fs %in% traces$fs])
-message(sprintf('Missing %s calls in Luscinia, showing first six', length(not_luscinia)))
-print(head(not_luscinia))
-st = st[!st$fs %in% not_luscinia,]
-
-# Names rows
-rownames(st) = st$fs
-
-# Run checks
-if(any(is.na(st$bird))) stop('Missing IDs!')
+# Subset st per year, include only calls with traces
+st_20 = subset.st(st_20, '2020', traces_20)
+st_21 = subset.st(st_21, '2021', traces_21)
 
 # Remove all traces that are not any longer in the selection tables and report them
-not_st = names(smooth_traces)[!names(smooth_traces) %in% st$fs]
+not_st = names(smooth_traces_20)[!names(smooth_traces_20) %in% st_20$fs]
 message(sprintf('Missing %s calls in selection tables, showing first six', length(not_st)))
-print(head(not_st))
-smooth_traces = smooth_traces[st$fs]
+if(length(not_st) > 0) print(head(not_st))
+smooth_traces_20 = smooth_traces_20[st_20$fs]
+
+not_st = names(smooth_traces_21)[!names(smooth_traces_21) %in% st_21$fs]
+message(sprintf('Missing %s calls in selection tables, showing first six', length(not_st)))
+if(length(not_st) > 0) print(head(not_st))
+smooth_traces_21 = smooth_traces_21[st_21$fs]
 
 # Adjust start and end times
-st$End.Time..s. = sapply(st$fs, function(fs)
-  st$Begin.Time..s.[st$fs == fs] + max(traces$Time[traces$fs == fs])/1000)
-st$Begin.Time..s. = sapply(st$fs, function(fs)
-  st$Begin.Time..s.[st$fs == fs] + min(traces$Time[traces$fs == fs])/1000)
-if(any(st$End.Time..s.-st$Begin.Time..s. > 3)) stop('Some calls are too long.')
-
-# Listing the call types to include - need to include more, just starting small
-source(path_call_type_classification)
+st_20 = adjust.times(st_20, traces_20)
+st_21 = adjust.times(st_21, traces_21)
 
 # Subset per call type and save
-data_sets = lapply(names(types_include), function(type) st$fs[st$`call type` %in% types_include[[type]]])
-names(data_sets) = names(types_include)
-loud_contact = path_sorted_loud_contact %>% list.files('*wav') %>% str_remove('.wav')
-loud_contact = loud_contact[loud_contact %in% data_sets$contact]
-data_sets$loud_contact = loud_contact
-
-# # Find isolated contact calls
-# sub_iso = st[st$`call type` == 'contact',]
-# sub_iso = sub_iso[sub_iso$context %in% c('isolated', 'response'),]
-# sub_iso = sub_iso[sub_iso$fs %in% traces$fs,]
-# 
-# # Add
-# data_sets = append(data_sets, list(isolated_contact = sub_iso$fs))
+data_sets_20 = create.data.sets(st_20, path_call_type_classification)
+data_sets_21 = create.data.sets(st_21, path_call_type_classification, path_sorted_loud_contact_21)
 
 # Load waves
 message('Loading waves...')
-waves = mclapply(1:nrow(st), function(i)
-  load.wave(path_audio_file = paste0(path_audio, '/', st$file[i], '.wav'), 
-            from = st$Begin.Time..s.[i],
-            to = st$End.Time..s.[i]), mc.cores = 4)
-names(waves) = st$fs
+waves_20 = mclapply(1:nrow(st_20), function(i)
+  load.wave(path_audio_file = paste0(path_audio_20, '/', st_20$file[i], '.wav'), 
+            from = st_20$Begin.Time..s.[i],
+            to = st_20$End.Time..s.[i]), mc.cores = 4)
+names(waves_20) = st_20$fs
+waves_21 = mclapply(1:nrow(st_21), function(i)
+  load.wave(path_audio_file = paste0(path_audio_21, '/', st_21$file[i], '.wav'), 
+            from = st_21$Begin.Time..s.[i],
+            to = st_21$End.Time..s.[i]), mc.cores = 4)
+names(waves_21) = st_21$fs
 message('Done!')
   
 # Save
-save(st, smooth_traces, data_sets, file = path_data)
-save(waves, file = path_waves)
+save(st_20, st_21, smooth_traces_20, smooth_traces_21, data_sets_20, data_sets_21, file = path_data)
+save(waves_20, waves_21, file = path_waves)
 message('Saved all data!')
