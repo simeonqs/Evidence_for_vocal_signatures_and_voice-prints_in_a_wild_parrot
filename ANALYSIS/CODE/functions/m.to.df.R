bc6119515fd0a28f535d192f5c875845edb3ff8f
Simeon <simeonqs@hotmail.com>
2022-01-29 13:16:59 +0100

    Replacing for loops with mclapply.

--- ANALYSIS/CODE/functions/m.to.df.R
+++ ANALYSIS/CODE/functions/m.to.df.R
@@ -1,7 +1,7 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: social networks
 # Date started: 24-08-2021
-# Date last modified: 28-01-2022
+# Date last modified: 29-01-2022
 # Author: Simeon Q. Smeele
 # Description: Taking matrix with distances and making it into a dataframe that can be analysed with stan 
 # model. 
@@ -12,6 +12,7 @@
 # This version made recordings unique per ind. 
 # This version includes the option to further clean the data. 
 # This version has the time difference on a log_10 scale rather than natural. 
+# This version uses mclapply with four threads rather than two for loops. 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 m.to.df = function(m, 
@@ -19,35 +20,33 @@ m.to.df = function(m,
                    recs = NULL,
                    time_saver = NULL,
                    day_saver = NULL,
-                   progress_bar = T,
                    clean_data = F){
   
-  d = data.frame()
-  
-  if(progress_bar) pb = txtProgressBar(min = 0, max = nrow(m), initial = 0, style = 3)
-  for(i in 1:nrow(m)){
-    if(progress_bar) setTxtProgressBar(pb,i)
-    if(i == ncol(m)) break
-    for(j in (i+1):ncol(m)){
-      new = data.frame(
-        d = m[i,j],
-        call_i = i,
-        call_j = j,
-        ind_i = inds[i],
-        ind_j = inds[j],
-        ind_pair = paste(inds[i], inds[j], sep = '-')
-      )
-      if(!is.null(recs)){
-        new = cbind(new, data.frame(
-          rec_i = paste(recs[i], inds[i]),
-          rec_j = paste(recs[j], inds[j]),
-          rec_pair = paste(recs[i], inds[i], recs[j], inds[j], sep = '-')))
-      }
-      if(!is.null(time_saver)) new$time = c(time_saver[i], time_saver[j]) %>% diff %>% abs %>% log10
-      if(!is.null(day_saver)) new$date = c(day_saver[i], day_saver[j]) %>% diff %>% abs
-      d = rbind(d, new)
+  l = nrow(m)
+  c = combn(1:l, 2)
+  d = mclapply(1:ncol(c), function(x) {
+    
+    new = data.frame(
+      d = m[c[1,x],c[2,x]],
+      call_i = c[1,x],
+      call_j = c[2,x],
+      ind_i = inds[c[1,x]],
+      ind_j = inds[c[2,x]],
+      ind_pair = paste(inds[c[1,x]], inds[c[2,x]], sep = '-')
+    )
+    if(!is.null(recs)){
+      new = cbind(new, data.frame(
+        rec_i = paste(recs[c[1,x]], inds[c[1,x]]),
+        rec_j = paste(recs[c[2,x]], inds[c[2,x]]),
+        rec_pair = paste(recs[c[1,x]], inds[c[1,x]], recs[c[2,x]], inds[c[2,x]], sep = '-')))
     }
-  }
+    if(!is.null(time_saver)) new$time = c(time_saver[c[1,x]], time_saver[c[2,x]]) %>% diff %>% abs %>% log10
+    if(!is.null(day_saver)) new$date = c(day_saver[c[1,x]], day_saver[c[2,x]]) %>% diff %>% abs
+    
+    return(new)
+    
+  }, mc.cores = 4) %>% bind_rows # end running through the combinations
+  
   d$ind_pair = as.integer(as.factor(d$ind_pair))
   if(!is.null(recs)){
     trans_recs = seq_along(unique(c(d$rec_i, d$rec_j)))
@@ -56,8 +55,7 @@ m.to.df = function(m,
     d$rec_j = trans_recs[d$rec_j]
     d$rec_pair = as.integer(as.factor(d$rec_pair))
   }
-  if(progress_bar) close(pb)
-  
+
   if(clean_data){
     clean_dat = as.list(d)
     clean_dat$d = as.numeric(scale(d$d))
