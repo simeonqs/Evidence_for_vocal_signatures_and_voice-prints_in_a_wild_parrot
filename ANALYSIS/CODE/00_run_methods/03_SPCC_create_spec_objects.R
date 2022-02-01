e0a187306147ef1af16ad2a12870b368eae99bf1
Simeon <simeonqs@hotmail.com>
2022-02-01 09:08:38 +0100

    Adding 2020 data.

--- ANALYSIS/CODE/00_run_methods/03_SPCC_create_spec_objects.R
+++ ANALYSIS/CODE/00_run_methods/03_SPCC_create_spec_objects.R
@@ -1,12 +1,13 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: chapter II
 # Date started: 26-08-2021
-# Date last modified: 25-01-2022
+# Date last modified: 31-01-2022
 # Author: Simeon Q. Smeele
 # Description: Creating spec objects for all calls from 2020.
 # This version adds the names to the spec_objects. 
 # This version is updated for the 2021 data. 
 # This version moves out the reading of the waves. 
+# This version was updated to include the 2020 data as well. 
 # source('ANALYSIS/CODE/00_run_methods/03_SPCC_create_spec_objects.R')
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
@@ -38,23 +39,30 @@ load(path_waves)
 # ANALYSIS: ----
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
+# Function to create spec objects
+create.spec.objects = function(waves){
+  message(sprintf('Starting processing of %s wave objects...', length(waves)))
+  spec_objects = sapply(waves, function(wave){
+    wave = ffilter(wave, from = 500, output = 'Wave')
+    spec_oject = cutted.spectro(wave, freq_range = c(1000, 3000), plot_it = F, 
+                                thr_low = 1.1, thr_high = 1.8,
+                                wl = 512, ovl = 450, 
+                                method = 'sd',
+                                sum_one = T)
+    return(spec_oject)
+  })
+  names(spec_objects) = names(waves)
+  message('Done!')
+  return(spec_objects)
+}
+
 # Generate spec_ojects
-message(sprintf('Starting processing of %s wave objects...', length(waves)))
-spec_objects = sapply(waves, function(wave){
-  wave = ffilter(wave, from = 500, output = 'Wave')
-  spec_oject = cutted.spectro(wave, freq_range = c(1000, 3000), plot_it = F, 
-                              thr_low = 1.1, thr_high = 1.8,
-                              wl = 512, ovl = 450, 
-                              method = 'sd',
-                              sum_one = T)
-  return(spec_oject)
-})
-message('Done!')
+spec_objects_20 = create.spec.objects(waves_20)
+spec_objects_21 = create.spec.objects(waves_21)
 
 # Plot example
-image(t(spec_objects[[1]]), col = hcl.colors(12, 'Blue-Yellow', rev = T)) 
+image(t(spec_objects_20[[1]]), col = hcl.colors(12, 'Blue-Yellow', rev = T)) 
 
 # Save spec_objects
-names(spec_objects) = names(waves)
-save(spec_objects, file = path_spec_objects)
+save(spec_objects_20, spec_objects_21, file = path_spec_objects)
 message('All objects are saved.')
