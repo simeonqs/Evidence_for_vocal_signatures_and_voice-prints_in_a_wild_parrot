abe901408063e34975408bc9b01e078bea990043
Simeon <simeonqs@hotmail.com>
2022-03-01 13:01:52 +0100

    Adding 2020 data.

--- ANALYSIS/CODE/save wavs all contact.R
+++ ANALYSIS/CODE/save wavs all contact.R
@@ -1,3 +1,11 @@
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# Project: voice paper
+# Date started: 28-02-2022
+# Date last modified: 28-02-2022
+# Author: Simeon Q. Smeele
+# Description: Saving contact calls so that they can be sorted into loud contact calls and others. 
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 # Loading libraries
 libraries = c('tidyverse', 'warbleR')
 for(lib in libraries){
@@ -10,7 +18,7 @@ rm(list=ls())
 
 # Paths
 source('ANALYSIS/CODE/paths.R')
-path_out = 'ANALYSIS/RESULTS/contact call sorting'
+path_out = 'ANALYSIS/RESULTS/contact call sorting 20'
 
 # Load data
 load(path_data)
@@ -18,29 +26,40 @@ load(path_waves)
 
 # Subset for contact calls
 source(path_call_type_classification)
-st = st[st$`call type` %in% types_include$contact,]
+# st_21 = st_21[st_21$`call type` %in% types_include$contact,]
+# 
+# # Get behaviours and subset for isolation calls
+# st_21 = st_21[which(st_21$context == 'single'),]
+# st_21 = st_21[which(!st_21$location %in% c('grass', 'ground')),]
+# st_21 = st_21[!is.na(st_21$behaviour),]
+# st_21 = st_21[!st_21r_detect(st_21$behaviour, 'response'),]
+# st_21 = st_21[!st_21r_detect(st_21$behaviour, 'responding'),]
+# st_21 = st_21[!st_21r_detect(st_21$behaviour, 'foraging'),]
+# st_21 = st_21[!st_21r_detect(st_21$behaviour, 'in beak'),]
+# st_21 = st_21[!st_21r_detect(st_21$behaviour, 'foraing'),]
+# st_21 = st_21[!st_21r_detect(st_21$behaviour, 'feeding'),]
+# st_21 = st_21[!st_21r_detect(st_21$behaviour, 'chewing on leaves'),]
+# st_21 = st_21[!st_21r_detect(st_21$behaviour, 'flying down to ground'),]
+# st_21 = st_21[!st_21r_detect(st_21$behaviour, 'flying down to the groundm'),]
+# st_21 = st_21[!st_21r_detect(st_21$behaviour, 'chewing branch'),]
+# unique_behaviours = st_21$behaviour %>% 
+#   st_21r_split(', ') %>% 
+#   unlist_21 %>% 
+#   st_21r_remove(' in') %>%
+#   unique
+# 
+# # Save wavs
+# for(i in 1:nrow(st_21)) writeWave(waves[st_21$fs[i]][[1]], 
+#                                sprintf('%s/%s.wav', path_out, st_21$fs[i]), 
+#                                extensible = F) 
 
-# Get behaviours and subset for isolation calls
-st = st[which(st$context == 'single'),]
-st = st[which(!st$location %in% c('grass', 'ground')),]
-st = st[!is.na(st$behaviour),]
-st = st[!str_detect(st$behaviour, 'response'),]
-st = st[!str_detect(st$behaviour, 'responding'),]
-st = st[!str_detect(st$behaviour, 'foraging'),]
-st = st[!str_detect(st$behaviour, 'in beak'),]
-st = st[!str_detect(st$behaviour, 'foraing'),]
-st = st[!str_detect(st$behaviour, 'feeding'),]
-st = st[!str_detect(st$behaviour, 'chewing on leaves'),]
-st = st[!str_detect(st$behaviour, 'flying down to ground'),]
-st = st[!str_detect(st$behaviour, 'flying down to the groundm'),]
-st = st[!str_detect(st$behaviour, 'chewing branch'),]
-unique_behaviours = st$behaviour %>% 
-  str_split(', ') %>% 
-  unlist %>% 
-  str_remove(' in') %>%
-  unique
+# Now doing 2020
+st = st_20[which(st_20$`call type` == 'contact'),]
+st = st[st$context == 'isolated',]
+st = st[st$behaviour %in% c('perched', 'perched, calls in background', 'perched, looking around',
+                            'perched nest tree, looking around', 'at nest', 'perched nest tree'),]
 
 # Save wavs
-for(i in 1:nrow(st)) writeWave(waves[st$fs[i]][[1]], 
-                               sprintf('%s/%s.wav', path_out, st$fs[i]), 
-                               extensible = F) 
+for(i in 1:nrow(st)) writeWave(waves_20[st$fs[i]][[1]],
+                               sprintf('%s/%s.wav', path_out, st$fs[i]),
+                               extensible = F)
