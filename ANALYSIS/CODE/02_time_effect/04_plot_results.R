5ccc96e74fc71e6de5f7684f701c19a12d005178
Simeon Smeele <simeonqs@hotmail.com>
2022-03-03 09:06:48 +0000

    Switching back to stanr format.

--- ANALYSIS/CODE/02_time_effect/04_plot_results.R
+++ ANALYSIS/CODE/02_time_effect/04_plot_results.R
@@ -1,7 +1,7 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: voice paper
 # Date started: 12-10-2021
-# Date last modified: 15-02-2022
+# Date last modified: 03-03-2022
 # Author: Simeon Q. Smeele
 # Description: Plotting model results per method.  
 # This version includes all call types. 
@@ -9,6 +9,7 @@
 # This version is updated for the 2021 data and new structure. 
 # This version switches to acoustic similarity rather than distance. 
 # This version also includes the 2020 data. 
+# This version switches back to proper posterior format. 
 # source('ANALYSIS/CODE/03_time_effect/04_plot_results.R')
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
@@ -36,21 +37,19 @@ load(path_data_sets_date)
 
 # Functions to plot
 plot.model.time = function(post, dat){
-  post_flat = apply(post, 3, rbind)
   points((dat$time + 2) * 7.5, -dat$d, pch = 16, col = alpha('purple', 0))
-  shade(-apply(sapply(seq_along(post_flat[,'a_bar']), 
+  shade(-apply(sapply(1:length(post[['a_bar']]), 
                      function(i) 
-                       post_flat[,'a_bar'][i] + c(-2, 2) * post_flat[,'b_bar'][i]), 1, PI),         
+                       post[['a_bar']][i] + c(-2, 2) * post[['b_bar']][i]), 1, PI),         
         (c(-2, 2) + 2) * 7.5, col = alpha('purple', 0.2))
-  lines((c(-2, 2) + 2) * 7.5, -(mean(post_flat[,'a_bar']) + c(-2, 2) * mean(post_flat[,'b_bar'])), 
+  lines((c(-2, 2) + 2) * 7.5, -(mean(post[['a_bar']]) + c(-2, 2) * mean(post[['b_bar']])), 
         col = alpha('purple', 1), lwd = 5, lty = 1)
 }
 plot.model.dates = function(post, dat){
-  post_flat = apply(post, 3, rbind)
-  shade(-apply(sapply(seq_along(post_flat[,'a_bar']), 
-                     function(i) post_flat[,'a_bar'][i] + c(0, 30) * post_flat[,'b_bar'][i]), 1, PI), 
+  shade(-apply(sapply(1:length(post[['a_bar']]), 
+                     function(i) post[['a_bar']][i] + c(0, 30) * post[['b_bar']][i]), 1, PI), 
         c(0, 30), col = alpha('darkorange', 0.1))
-  lines(c(0, 30), -(mean(post_flat[,'a_bar']) + c(0, 30) * mean(post_flat[,'b_bar'])), 
+  lines(c(0, 30), -(mean(post[['a_bar']]) + c(0, 30) * mean(post[['b_bar']])), 
         col = alpha('darkorange', 1), lwd = 5, lty = 1)
 }
 write.title = function(label){
@@ -62,12 +61,12 @@ write.title = function(label){
 }
 
 # Order call types
-call_types = c('contact', 'loud_contact', 'short_contact', 'trruup', 'tja', 'alarm', 'growl', 'growl_low')
+call_types = c('contact', 'loud_contact', 'short_contact', 'trruup', 'tja', 'tjup', 'alarm', 'growl', 'growl_low')
 
 # Plot beta parameter per call type
 {
-  pdf(path_pdf_time_results, 16, 9)
-  par(mfrow = c(4, 9), oma = c(2, 0, 2, 1), mgp = c(1, 0.75, 0))
+  pdf(path_pdf_time_results, 18, 9)
+  par(mfrow = c(4, 10), oma = c(2, 0, 2, 1), mgp = c(1, 0.75, 0))
   
   for(year in c(20, 21)){
     
@@ -78,7 +77,7 @@ call_types = c('contact', 'loud_contact', 'short_contact', 'trruup', 'tja', 'ala
     all_models_out_time = get(sprintf('all_models_out_time_%s', year))
     
     write.title('DTW')
-    for(type in c('contact', 'loud_contact', 'short_contact', 'trruup', 'tja')){
+    for(type in c('contact', 'loud_contact', 'short_contact', 'trruup', 'tja', 'tjup')){
       if(type == 'loud_contact' & year == 20){
         plot.new()
         mtext(str_replace(call_types[2], '_', ' '), 3, 1, font = 2)
@@ -86,11 +85,11 @@ call_types = c('contact', 'loud_contact', 'short_contact', 'trruup', 'tja', 'ala
       }
       plot(data_sets_date$dtw[[type]]$date, -data_sets_date$dtw[[type]]$d, 
            pch = 16, col = alpha('darkorange', 0),
-           xlim = c(0, 30), ylim = c(-2, 2),
+           xlim = c(0, 30), ylim = c(-3, 3),
            xlab = '', ylab = '', xaxt = 'n', yaxt = 'n')
-      plot.model.time(all_models_out_time$dtw[[type]], 
+      plot.model.time(all_models_out_time$dtw[[type]]$post, 
                       data_sets_time$dtw[[type]])
-      plot.model.dates(all_models_out_date$dtw[[type]], 
+      plot.model.dates(all_models_out_date$dtw[[type]]$post, 
                        data_sets_date$dtw[[type]])
       if(type == call_types[1]){
         axis(2)
@@ -99,11 +98,11 @@ call_types = c('contact', 'loud_contact', 'short_contact', 'trruup', 'tja', 'ala
       mtext(str_replace(type, '_', ' '), 3, 1, font = 2)
     }
     plot.new()
-    mtext(str_replace(call_types[6], '_', ' '), 3, 1, font = 2)
-    plot.new()
     mtext(str_replace(call_types[7], '_', ' '), 3, 1, font = 2)
     plot.new()
     mtext(str_replace(call_types[8], '_', ' '), 3, 1, font = 2)
+    plot.new()
+    mtext(str_replace(call_types[9], '_', ' '), 3, 1, font = 2)
     
     write.title('SPCC')
     for(type in call_types){
@@ -115,9 +114,9 @@ call_types = c('contact', 'loud_contact', 'short_contact', 'trruup', 'tja', 'ala
            pch = 16, col = alpha('darkorange', 0),
            xlim = c(0, 30), ylim = c(-2, 2),
            xlab = '', ylab = '', xaxt = 'n', yaxt = 'n')
-      plot.model.time(all_models_out_time$spcc[[type]], 
+      plot.model.time(all_models_out_time$spcc[[type]]$post, 
                       data_sets_time$spcc[[type]])
-      plot.model.dates(all_models_out_date$spcc[[type]], 
+      plot.model.dates(all_models_out_date$spcc[[type]]$post, 
                        data_sets_date$spcc[[type]])
       if(type == call_types[1]){
         axis(2)
@@ -139,9 +138,9 @@ call_types = c('contact', 'loud_contact', 'short_contact', 'trruup', 'tja', 'ala
            pch = 16, col = alpha('darkorange', 0),
            xlim = c(0, 30), ylim = c(-2, 2),
            xlab = '', ylab = '', xaxt = 'n', yaxt = 'n')
-      plot.model.time(all_models_out_time$specan[[type]], 
+      plot.model.time(all_models_out_time$specan[[type]]$post, 
                       data_sets_time$specan[[type]])
-      plot.model.dates(all_models_out_date$specan[[type]], 
+      plot.model.dates(all_models_out_date$specan[[type]]$post, 
                        data_sets_date$specan[[type]])
       if(type == call_types[1]){
         axis(2)
@@ -159,9 +158,9 @@ call_types = c('contact', 'loud_contact', 'short_contact', 'trruup', 'tja', 'ala
            pch = 16, col = alpha('darkorange', 0),
            xlim = c(0, 30), ylim = c(-2, 2),
            xlab = '', ylab = '', xaxt = 'n', yaxt = 'n', main = '')
-      plot.model.time(all_models_out_time$mfcc[[type]], 
+      plot.model.time(all_models_out_time$mfcc[[type]]$post, 
                       data_sets_time$mfcc[[type]])
-      plot.model.dates(all_models_out_date$mfcc[[type]], 
+      plot.model.dates(all_models_out_date$mfcc[[type]]$post, 
                        data_sets_date$mfcc[[type]])
       if(type == call_types[1]){
         axis(2)
