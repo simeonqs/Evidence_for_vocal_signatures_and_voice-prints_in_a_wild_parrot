faffc7513a81eae0c5f4712325459f1ff144a25b
Simeon <simeonqs@hotmail.com>
2022-03-07 11:27:16 +0100

    Minor update.

--- ANALYSIS/CODE/04_cross_call_type/00_MFCC_run.R
+++ ANALYSIS/CODE/04_cross_call_type/00_MFCC_run.R
@@ -1,7 +1,7 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: voice paper
 # Date started: 06-02-2022
-# Date last modified: 06-02-2022
+# Date last modified: 03-03-2022
 # Author: Simeon Q. Smeele
 # Description: Running mfcc per call type and saving data for DFA. 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -30,8 +30,12 @@ waves = append(waves_20, waves_21)
 
 # Run mfcc
 message(sprintf('Running mfcc on %s waves...', length(waves)))
-mfcc_out = lapply(waves, run.mfcc)
+mfcc_out = lapply(waves, run.mfcc, numcep = 15)
+message(sprintf('Running mfcc on %s waves...', length(waves_20)))
+mfcc_out_20 = lapply(waves_20, run.mfcc, numcep = 15)
+message(sprintf('Running mfcc on %s waves...', length(waves_21)))
+mfcc_out_21 = lapply(waves_21, run.mfcc, numcep = 15)
 
 # Save
-save(mfcc_out, file = path_mfcc_out)
+save(mfcc_out, mfcc_out_20, mfcc_out_21, file = path_mfcc_out)
 message('Done.')
