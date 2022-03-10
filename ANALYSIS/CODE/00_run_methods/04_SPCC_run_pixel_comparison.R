04a6b0ce000f3d1de03f70b76077c5ab1a58cce5
Simeon Smeele <simeon_smeele@ecocn05.eva.mpg.de>
2022-03-10 08:38:27 +0100

    Combining data from years.

--- ANALYSIS/CODE/00_run_methods/04_SPCC_run_pixel_comparison.R
+++ ANALYSIS/CODE/00_run_methods/04_SPCC_run_pixel_comparison.R
@@ -1,7 +1,7 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: chapter II
 # Date started: 26-08-2021
-# Date last modified: 02-02-2022
+# Date last modified: 08-02-2022
 # Author: Simeon Q. Smeele
 # Description: Running pixel comparison on all calls from 2020. 
 # Note: This script takes a long time. Best to run on HPC and use many cores.
@@ -9,6 +9,7 @@
 # This version adds the file_sels to the o object. 
 # This version is updated to run with the 2021 data and run smoother. 
 # This version is updates to include the 2020 data. 
+# This version combines data from both years. 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # Loading libraries
@@ -46,11 +47,9 @@ run.spcc = function(data_set, spec_objects){
   rownames(m) = colnames(m) = data_set
   return(m)
 }
-m_list_20 = lapply(data_sets_20, run.spcc, spec_objects_20)
-names(m_list_20) = names(data_sets_20)
-m_list_21 = lapply(data_sets_21, run.spcc, spec_objects_21)
-names(m_list_21) = names(data_sets_21)
+m_list = lapply(data_sets, run.spcc, spec_objects)
+names(m_list) = names(data_sets)
 
 # Save
-save(m_list_20, m_list_21, file = path_spcc_m_list)
+save(m_list, file = path_spcc_m_list)
 message('Done!')
