f5d92db1ad4d423f05394b45e51860e555a6a027
Simeon <simeonqs@hotmail.com>
2022-03-21 08:58:09 +0100

    Removing across year.

--- ANALYSIS/CODE/vectorised srm/03_run_real_data.R
+++ ANALYSIS/CODE/vectorised srm/03_run_real_data.R
@@ -1,7 +1,7 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: voice paper
 # Date started: 17-03-2022
-# Date last modified: 17-03-2022
+# Date last modified: 18-03-2022
 # Author: Simeon Q. Smeele
 # Description: Preparing the real data to test the vectorised srm.
 # source('ANALYSIS/CODE/vectorised srm/03_run_real_data.R')
@@ -30,28 +30,23 @@ load(path_data)
 load(path_dtw_m_list)
 
 # Prep data
+set.seed(1)
 n = rownames(m_list$contact)
 subber = 1:length(n)
-if(length(n) > 100) subber = sample(length(n), 100) else subber = 1:length(n)
+if(length(n) > 200) subber = sample(length(n), 200) else subber = 1:length(n)
 inds = st[n,]$bird[subber] %>% as.factor %>% as.integer
 recs = paste(st[n,]$bird[subber], st[n,]$file[subber]) %>% as.factor %>% as.integer
 m =  m_list$contact[subber, subber]
 
 # Make vectors
 ## acoustic distance
-clean_dat = list(acc_dist = as.vector(scale(as.vector(as.dist(m)))))
+clean_dat = data.frame(acc_dist = as.vector(scale(as.vector(as.dist(m)))))
 ## getting dimensions
 l = nrow(m)
 c = combn(1:l, 2)
 ## call
 clean_dat$call_i = c[1,]
 clean_dat$call_j = c[2,]
-## ind
-clean_dat$ind_i = inds[c[1,]]
-clean_dat$ind_j = inds[c[2,]]
-## rec
-clean_dat$rec_i = recs[c[1,]]
-clean_dat$rec_j = recs[c[2,]]
 ## same ind = 1, else = 0
 clean_dat$same_ind = sapply(1:ncol(c), function(x) ifelse(inds[c[1,x]] == inds[c[2,x]], 1, 2))
 ## same rec = 1, else = 0
@@ -62,11 +57,21 @@ clean_dat$ind_pair = sapply(1:ncol(c), function(x)
 ## rec pair
 clean_dat$rec_pair = sapply(1:ncol(c), function(x) 
   paste(sort(c(recs[c[1,x]], recs[c[2,x]])), collapse = ' ')) |> as.factor() |> as.integer()
+
+# Remove across years
+year_1 = paste(st[n,]$bird[subber], st[n,]$file[subber])[c[1,]] %>% str_split(' ') %>% sapply(`[`, 2) %>% 
+  str_split('_') %>% sapply(`[`, 1)
+year_2 = paste(st[n,]$bird[subber], st[n,]$file[subber])[c[2,]] %>% str_split(' ') %>% sapply(`[`, 2) %>% 
+  str_split('_') %>% sapply(`[`, 1)
+clean_dat = clean_dat[year_1 == year_2,]
+clean_dat$ind_pair = as.integer(as.factor(clean_dat$ind_pair))
+clean_dat$rec_pair = as.integer(as.factor(clean_dat$rec_pair))
+
+# Add sample sizes
+clean_dat = as.list(clean_dat)
 clean_dat$N_ind_pair = max(clean_dat$ind_pair)
 clean_dat$N_rec_pair = max(clean_dat$rec_pair)
 clean_dat$N_call = max(clean_dat$call_j)
-clean_dat$N_ind = max(clean_dat$ind_j)
-clean_dat$N_rec = max(clean_dat$rec_j)
 clean_dat$N_obs = length(clean_dat$call_i)
 
 # Run model and save
