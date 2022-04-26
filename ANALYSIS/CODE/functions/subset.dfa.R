955dcaf7dfc05b4064fe7c381bd6e4265961f214
Simeon <simeonqs@hotmail.com>
2022-04-26 09:56:32 +0200

    Adding test code and max_iter option.

--- ANALYSIS/CODE/functions/subset.dfa.R
+++ ANALYSIS/CODE/functions/subset.dfa.R
@@ -1,17 +1,19 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: voice paper
 # Date started: 15-02-2022
-# Date last modified: 02-03-2022
+# Date last modified: 25-04-2022
 # Author: Simeon Q. Smeele
 # Description: Subsets recordings per individual for DFA.  
 # This version includes options for test and train set. 
 # This version includes the option the balance the data set. 
+# This version includes more test code and the max_iter can be set. 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 subset.dfa = function(N_train, N_test, 
                       train_set, test_set, 
                       st,
-                      balance = T){
+                      balance = T,
+                      max_iter = 1000){
   
   # Subset recordings
   inds = unique(st$bird)
@@ -53,23 +55,36 @@ subset.dfa = function(N_train, N_test,
         recs_test = recs_train = c()
         n_test = 0
       } 
-      if(ii > 1000) cont = F # stop if not able to resolve
+      if(ii > max_iter) cont = F # stop if not able to resolve
       if(n_test >= N_test & n_train >= N_train){cont = F; inds_include = c(inds_include, ind)}
     }
     
-    # Test for problems
-    if(any(recs_train %in% recs_test) | any(recs_test %in% recs_train)) 
-      stop('Some recordings go across sets!')
+    # Find names file sels
+    new_names_train = st$fs[which(st$bird == ind & st$main_type %in% train_set & 
+                                    st$file %in% recs_train)]
+    new_names_test = st$fs[which(st$bird == ind & st$main_type %in% test_set & 
+                                   st$file %in% recs_test)]
     
     # If successful save the train and test recording names
     if(ind %in% inds_include){
+      
+      # Test for problems
+      if(any(recs_train %in% recs_test) | any(recs_test %in% recs_train)) 
+        stop('Some recordings go across sets!')
+      if(any(sapply(recs_test, function(rec) str_detect(new_names_train, rec))) | 
+         any(sapply(recs_train, function(rec) str_detect(new_names_test, rec))) )
+        stop('Some recordings go across sets!')
+      if(any(!st$main_type[st$fs %in% new_names_train] %in% train_set) | 
+         any(!st$main_type[st$fs %in% new_names_test] %in% test_set)) 
+        stop('Wrong type in set!')
+      
+      # Save
       names_train = c(names_train,
-                      st$fs[which(st$bird == ind & st$main_type %in% train_set & 
-                                    st$file %in% recs_train)])
+                      new_names_train)
       names_test = c(names_test, 
-                     st$fs[which(st$bird == ind & st$main_type %in% test_set & 
-                                   st$file %in% recs_test)])
-    }
+                     new_names_test)
+      
+    } # end if loop
     
   } # end ind loop
   
@@ -87,7 +102,10 @@ subset.dfa = function(N_train, N_test,
     names_test = names_test_slim
   }
   
+  # Test for problems
   if(any(names_train %in% names_test) | any(names_test %in% names_train)) 
+    stop('Some calls go across sets!')
+  if(any(names_train_slim %in% names_test) | any(names_test_slim %in% names_train)) 
     stop('Some calls go across sets!')
   
   return(list(names_train = names_train, 
