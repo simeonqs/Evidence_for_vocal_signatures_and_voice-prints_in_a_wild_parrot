bbae86be89c30f837e506358b01eee022599e8aa
Simeon <simeonqs@hotmail.com>
2022-04-27 09:53:39 +0200

    Minor updates.

--- ANALYSIS/CODE/01_compare_call_types/04_test_vis.R
+++ ANALYSIS/CODE/01_compare_call_types/04_test_vis.R
@@ -3,7 +3,7 @@
 # Date started: 27-04-2022
 # Date last modified: 27-04-2022
 # Author: Simeon Q. Smeele
-# Description: Testing new way of visulisation. 
+# Description: Testing new way of visualisation. 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # Loading libraries
@@ -19,11 +19,16 @@ rm(list=ls())
 # Paths
 source('ANALYSIS/CODE/paths.R')
 
-# Import functions
-# .functions = sapply(list.files(path_functions, pattern = '*R', full.names = T), source)
-
 # Load data
 load(path_data)
 
 # List models
 models = list.files(str_remove(path_out_vsrm, '/vsrm'), '*vsrm*', full.names = T)
+
+# Load and plot
+load(models[2])
+
+
+
+
+
