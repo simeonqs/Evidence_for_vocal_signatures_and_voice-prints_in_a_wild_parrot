fe09b980d44cfc9f62566afc89d6c5a4e997a95a
Simeon <simeonqs@hotmail.com>
2022-05-14 15:00:30 +0200

    Adding option to randomise within area.

--- ANALYSIS/CODE/functions/run.dfa.R
+++ ANALYSIS/CODE/functions/run.dfa.R
@@ -1,12 +1,13 @@
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Project: voice paper
 # Date started: 15-02-2022
-# Date last modified: 23-04-2022
+# Date last modified: 09-05-2022
 # Author: Simeon Q. Smeele
 # Description: Running DFA. Assumes multiple objects are loaded, very specific to this chapter. 
+# This version has to option to randomise within nesting locations. 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-run.dfa = function(names_train, names_test, mfcc_out, print_scaling = T){
+run.dfa = function(names_train, names_test, mfcc_out, permute = F, print_scaling = T){
   
   # Prepare data
   mfcc_scale = scale(bind_rows(mfcc_out))
@@ -29,15 +30,24 @@ run.dfa = function(names_train, names_test, mfcc_out, print_scaling = T){
   # print(ggplot(lda_data, aes(LD1, LD2)) +
   #         geom_point(aes(color = inds)))
   
-  # Print scaling
+  # Save scaling
   scaling = model$scaling
   first = names(which(abs(scaling[,1]) == max(abs(scaling[,1]))))
   
   # Do randomised
   mfcc_random_train = mfcc_train
-  mfcc_random_train$inds = sample(mfcc_random_train$inds)
   mfcc_random_test = mfcc_test
-  mfcc_random_test$inds = sample(mfcc_random_test$inds)
+  if(permute){
+    for(n in unique(st$nesting)){
+      mfcc_random_train$inds[st[names_train,]$nesting == n] = 
+        sample(mfcc_random_train$inds[st[names_train,]$nesting == n])
+      mfcc_random_test$inds[st[names_test,]$nesting == n] = 
+        sample(mfcc_random_test$inds[st[names_test,]$nesting == n])
+    }
+  } else {
+    mfcc_random_train$inds = sample(mfcc_random_train$inds)
+    mfcc_random_test$inds = sample(mfcc_random_test$inds)
+  }
   model = lda(inds ~ ., data = mfcc_random_train)
   predictions = model %>% predict(mfcc_random_test)
   score_random = mean(predictions$class == mfcc_random_test$inds)
